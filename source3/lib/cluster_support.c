509d6e71656a72904c27fa45d0add03bb647c780
SilvanScherrer <silvan.scherrer@aroa.ch>
2018-03-23 09:47:08 +0100

    hack alert!, this needs to be redone later on

--- source3/lib/cluster_support.c
+++ source3/lib/cluster_support.c
@@ -59,10 +59,12 @@ const char *lp_ctdbd_socket(void)
 {
 	const char *ret;
 
+#ifndef __OS2__  //@todo bww resolve it eventually in the build system
 	ret = lp__ctdbd_socket();
 	if (ret != NULL && strlen(ret) > 0) {
 		return ret;
 	}
+#endif
 
 #ifdef CTDB_SOCKET
 	return CTDB_SOCKET;
