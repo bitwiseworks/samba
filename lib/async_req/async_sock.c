c541f26cb97cde72d2d21ad32ef1b9001309c6ff
SilvanScherrer <silvan.scherrer@aroa.ch>
2019-02-15 17:02:53 +0100

    set nonblocking with set_blocking() as well, as fcntl sometimes is not working

--- lib/async_req/async_sock.c
+++ lib/async_req/async_sock.c
@@ -170,6 +170,11 @@ static void async_connect_cleanup(struct tevent_req *req,
 		if (ret == -1) {
 			abort();
 		}
+#ifdef __OS2__ // if the old flags have blocking set we need to set it also (fcntl bug)
+	if (!(state->old_sockflags & O_NONBLOCK)) {
+        	set_blocking(state->fd, true);
+	}
+#endif
 
 		state->fd = -1;
 	}
